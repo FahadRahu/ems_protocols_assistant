cef930c5d9232a5afd76a1ac9c384cc30b3d1efb
Fahad Rahu <fahadrahu@gmail.com>
2024-09-05 01:08:46 -0400

    Created General Patient Care - Adult Protocol
    Not finished, but made an avpu_checker to assess for mental status.
    Added a cardiac_presence check as per early steps of the respective protocol.
    
    The rest of the protocol isn't near finished. Need to eventually embed protocol modules into this one.
    I.e. the cardiac arrest one, right now it just says do CPR.
    
    Figure out a way to reference that (eventually) made module, or how to organize and go about jumping there
    
    Need to figure out an organized logic to reference all the different protocols
    Otherwise main.py is going to be very confusing.

---
+++ protocols/GeneralPCP-Adult.py
@@ -0,0 +1,41 @@
+# General Patient Care Protocol - Adult
+def avpu_check():
+    print('Input the mental status of the patient:\n'
+          'A = Alert & Oriented: Patient is Awake and Alert \n'
+          'V = Verbal: Patient Responds to Verbal Stimulus \n'
+          'P = Pain: Patient Responds to Painful Stimulus \n'
+          'U = Unresponsive: Patient Responds to Unresponsive Stimulus \n')
+    avpu_status = input('Enter Patient Mental Status (A/V/P/U): ')
+    avpu_status = avpu_status.upper()
+    while avpu_status not in ['A', 'V', 'P', 'U']:
+        print('Invalid input, enter A/V/P/U\n')
+        avpu_status = input('Enter Patient Mental Status (A/V/P/U): ')
+        avpu_status = avpu_status.upper()
+    if avpu_status == 'A':
+        avpu_score = 4
+    elif avpu_status == 'V':
+        avpu_score = 3
+    elif avpu_status == 'P':
+        avpu_score = 2
+    else:
+        avpu_score = 1
+
+
+def general_cardiac ():
+    print('Check for a central pulse.')
+    pulse_presence = input('Does the patient have a pulse (Y/N): ')
+    pulse_presence = pulse_presence.upper()
+    while pulse_presence not in ['Y', 'N']:
+        print('Invalid input, enter Y/N\n')
+        pulse_presence = input('Does the patient have a pulse (Y/N): ')
+        pulse_presence = pulse_presence.upper()
+    if pulse_presence == 'N':
+        # GO TO CPR - UNCODED PROTOCOL
+        print('Refer to Cardiac Arrest: General Approach')
+        print('Start CPR')
+        return
+    else:
+        print('Assess the patient\'s perfusion: \n\n'
+              'Measure Heart Rate, Skin Color, Capillary Refill, Quality of Pulses')
+    print('g')
+general_cardiac()
