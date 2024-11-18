3db5a8b737565c386d19590206ac5cf84bc427b7
FahadRahu <fahadrahu@gmail.com>
2024-11-18 00:17:52 -0500

    Amended medical_protocols functions
    Edited altered mental status functions for both adult and pediatric to use a list plus enumerate 'for' loop instead of printing each step.
    Added docstrings to functions
    Added placeholder functions for diabetic emergencies protocols with notes

--- protocols/medical_protocols.py
+++ protocols/medical_protocols.py
@@ -3,6 +3,10 @@ from procedures import other_clinical_procedures
 
 # BOTH Adult and Pediatric Protocols Available:
 def bls_adult_medical_allergic_reaction():
+    """Protocol for altered mental status - needs functions [i.e. was a 12-lead performed? Did you take BGl?]
+    :return: Nothing as of right now
+    Plans: Need to add functions to return if any interventions were performed.
+    """
     print(f"1. Follow the General Patient Care Protocol for Adult patients.\n"
           "2. Assist the patient in self-administration of their prescribed epinephrine auto-injector "
           "(Epi-Pen/Epi-Pen Jr.)\n"
@@ -19,17 +23,72 @@ def bls_pediatric_medical_allergic_reaction():
           "4. Do not give the patient anything by mouth.")
 
 
-# Unfinished - Needs functions to refer to many different protocols.
+# UNFINISHED - Needs functions if any interventions were performed.
 def bls_adult_alt_mental_status_syncope():
-    print("Follow the advice consistent with the General Patient Care Protocol - Adult\n")
-    print("Obtain Blood Glucose Level - Refer to Medical: Diabetic Emergencies")
-    print("Obtain a 12-Lead ECG - \n"
-          "If head injury is suspected - refer to Trauma: Head Injuries.\n"
-          "If severely agitated and/or violent - refer to Medical: Behavioral Emergencies/Excited Delirium\n"
-          "If sepsis is suspected (high in age, high risk for infection, febrile/showing fever symptoms - "
-          "refer to - Medical: Sepsis\n"
-          "If you suspect poisoning - refer to Overdose and Poisoning: General Approach")
+    """Protocol = Adult Altered Mental Status
+    Why Unfinished?: Needs functions [i.e. was a 12-lead performed? Did you take BGl?]
+    :return: Nothing as of right now
+    Plans: Need to add functions to return if any interventions were performed.
+    """
 
+    altered_guide = [
+        "Follow the advice consistent with the General Patient Care Protocol - Pediatric",
+        "Obtain Blood Glucose Level - Refer to Medical: Diabetic Emergencies",
+        "Obtain a 12-Lead ECG",
+        "If head injury is suspected - refer to Trauma: Head Injuries.",
+        "If severely agitated and/or violent - refer to Medical: Behavioral Emergencies/Excited Delirium",
+        "If sepsis is suspected (greater age, high risk for infection, febrile/showing fever symptoms - "
+        "refer to - Medical: Sepsis",
+        "If you suspect poisoning - refer to Overdose and Poisoning: General Approach"
+    ]
+
+    for i, guideline in enumerate(altered_guide, start=1):
+        print(f"{i}. {guideline}")
+
+
+# UNFINISHED - Needs functions if any interventions were performed.
+def bls_pediatric_alt_mental_status_syncope():
+    """Protocol = Pediatric Altered Mental Status
+    Why Unfinished?: Needs functions [i.e. was a 12-lead performed? Did you take BGl?]
+    :return: Nothing as of right now
+    Plans: Need to add functions to return if any interventions were performed.
+    """
+    altered_guide = [
+        "Follow the advice consistent with the General Patient Care Protocol - Pediatric",
+        "Obtain Blood Glucose Level - Refer to Medical: Diabetic Emergencies",
+        "Obtain a 12-Lead ECG",
+        "If head injury is suspected - refer to Trauma: Head Injuries",
+        "If sepsis is suspected (greater age, high risk for infection, febrile/showing fever symptoms - "
+        "refer to - Medical: Sepsis",
+        "If you suspect poisoning - refer to Pediatric Overdose and Poisoning"
+    ]
+    for i, guideline in enumerate(altered_guide, start=1):
+        print(f"{i}. {guideline}")
+
+
+def bls_adult_diabetic_emergencies(local_age_type):
+    """Protocol = Adult Diabetic Emergencies
+    Why Unfinished?: Nothing is coded - Need to add function if Oral Glucose Gel or sugar was administered
+    :return: Nothing as of right now
+    Plans: Need to code the protocol, need to return whether oral glucose or sugary food/bev was given
+    Other Notes: For now, I'll leave it as 'print{whatever the protocol is}' and make it more complex later.
+    Hypoglycemia in ALL ADULTS IS <= 60 mg/dL
+    Hyperglycemia in ALL ADULTS IS >= 250 mg/dL
+    """
+    pass
+
+
+def bls_pediatric_diabetic_emergencies(local_age_type):
+    """Protocol = Pediatric Diabetic Emergencies
+    Why Unfinished?: Nothing is coded - Need to add function if Oral Glucose Gel or sugar was administered
+    :return: Nothing as of right now
+    Plans: Need to code the protocol, need to return whether oral glucose or sugary food/bev was given
+    Other Notes: For now, I'll leave it as 'print{whatever the protocol is}' and make it more complex later.
+    Hypoglycemia in a neonate is <= 40 mg/dL
+    Hypoglycemia in ALL OTHER PEDIATRICS is <= 60 mg/dL
+    Hyperglycemia in ALL PEDIATRICS is >= 250 mg/dL
+    """
+    pass
 
 # ONLY Adult Protocols Available:
 
